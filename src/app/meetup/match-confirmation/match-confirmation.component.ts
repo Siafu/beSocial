3a91389c97125d80f3c49dfd2c4c278845222120
Kiril Pepeljugoski <kiril.pepeljugoski@nera.com>
2019-05-31 13:42:59 -0400

    component start

---
+++ src/app/meetup/match-confirmation/match-confirmation.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Page } from "tns-core-modules/ui/page/page";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { RouterExtensions } from "nativescript-angular/router";
+import { TextField } from "tns-core-modules/ui/text-field";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'ns-match-confirmation',
+  templateUrl: './match-confirmation.component.html',
+  styleUrls: ['./match-confirmation.component.css'],
+  moduleId: module.id,
+})
+export class MatchConfirmationComponent implements OnInit {
+  meetingInfo = {
+
+    matchedTo: {
+      name: 'Luffy',
+      profilePic: 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/monkey-d-luffy-one-piece-53.3.jpg'
+    },
+    meetingDetails: 'the details',
+    Date: '05/31/2019',
+    Time: '3:50pm',
+    Location: 'NYC',
+    Comments: 'its a test'
+
+  }
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
