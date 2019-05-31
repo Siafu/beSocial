36f522200fc3087c389f466f3db3cf5b043ff905
Kambui Nurse <kambui.nurse@nera.com>
2019-05-31 08:51:05 -0400

    add ui service logic

--- src/app/shared/ui/ui.service.ts
+++ src/app/shared/ui/ui.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ViewContainerRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
 export class UiService {
+    constructor() {}
+    private _drawerState = new BehaviorSubject<void>(null);
+    private _rootVCRef: ViewContainerRef;
 
-  constructor() { }
+    get drawerState() {
+        return this._drawerState.asObservable();
+    }
+
+    toggleDrawer() {
+        this._drawerState.next(null);
+    }
+
+    setRootVCRef(vcRef: ViewContainerRef) {
+        this._rootVCRef = vcRef;
+    }
+    getRootVCRef() {
+        return this._rootVCRef;
+    }
 }
