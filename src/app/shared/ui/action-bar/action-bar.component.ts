67aff5e9c16d9e37d8e5d886c607eaff44235434
Kambui Nurse <kambui.nurse@nera.com>
2019-05-31 08:49:08 -0400

    add action bar logic

--- src/app/shared/ui/action-bar/action-bar.component.ts
+++ src/app/shared/ui/action-bar/action-bar.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Page } from "tns-core-modules/ui/page";
+import { UiService } from "../ui.service";
+import { RouterExtensions } from "nativescript-angular/router";
+import { isAndroid } from "tns-core-modules/platform";
+declare var android: any;
 @Component({
-  selector: 'ns-action-bar',
-  templateUrl: './action-bar.component.html',
-  styleUrls: ['./action-bar.component.css'],
-  moduleId: module.id,
+    selector: "ns-action-bar",
+    templateUrl: "./action-bar.component.html",
+    styleUrls: ["./action-bar.component.css"],
+    moduleId: module.id
 })
 export class ActionBarComponent implements OnInit {
+    constructor(
+        private page: Page,
+        private uiservice: UiService,
+        private router: RouterExtensions
+    ) {}
 
-  constructor() { }
+    @Input() title: string;
+    @Input() hasMenu: string;
 
-  ngOnInit() {
-  }
+    get android() {
+        return isAndroid;
+    }
 
+    onGoBack() {
+        this.router.backToPreviousPage();
+    }
+
+    onLoadedActionBar() {
+        if (isAndroid) {
+            const andrioidToolbar = this.page.actionBar.nativeView;
+            const backButton = andrioidToolbar.getNavigationIcon();
+
+            if (backButton) {
+                backButton.setColorFilter(
+                    android.graphics.Color.parseColor("#171717"),
+                    (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
+                );
+            }
+        }
+    }
+    onToggleMenu() {
+        this.uiservice.toggleDrawer();
+    }
+
+    ngOnInit() {}
 }
