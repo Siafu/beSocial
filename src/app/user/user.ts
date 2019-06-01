829ee6bd628c9a98cc147357d10a6fd9a470d893
Kambui Nurse <kambui.nurse@nera.com>
2019-06-01 14:47:32 -0400

    add Meeting type to user

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,10 +1,16 @@
 import { Interest } from './interest';
 
+export interface Meeting {
+    id: number;
+    name: string;
+    description: string;
+}
 export interface User {
     id: number;
     name: string;
     title: string;
     email: string;
     interests: Array<Interest>
+    meetings: Array<Meeting>
 }
 
