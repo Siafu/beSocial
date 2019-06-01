cfffc995d39966249c2827ebd3c7d99f44a5beae
Kambui Nurse <kambui.nurse@nera.com>
2019-06-01 16:54:11 -0400

    finish up user detail component

--- src/app/user/user-detail.component.ts
+++ src/app/user/user-detail.component.ts
@@ -4,22 +4,24 @@ import { UserService } from './user.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'ns-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css'],
-  moduleId: module.id,
+    selector: "ns-user-detail",
+    templateUrl: "./user-detail.component.html",
+    styleUrls: ["./user-detail.component.css"],
+    moduleId: module.id
 })
 export class UserDetailComponent implements OnInit {
-
     user: User;
 
     constructor(
         private userService: UserService,
         private route: ActivatedRoute
-    ) { }
+    ) {}
 
     ngOnInit(): void {
         const id = +this.route.snapshot.params.id;
         this.user = this.userService.getUser(id);
+    }
+    onSubmit() {
+        return false;
     }
 }
